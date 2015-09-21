130a68e5d6a9aebf77e223f3e888a3de6d2b734b
Hiroyuki-Shinoe <yuxing0129@gmail.com>
2015-09-21 06:58:40 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
     def home
+        # データベーステーブルへのアクセスには、１文字目の大文字にしたテーブル名を使う
+        # findメソッドはデータベースを検索するためのもの。
+        # parameterとしてIDを渡して、合致するものを探す
+        @basic_plan = Plan.find(1)
+        # idが１のデータが入っているインスタンス変数 @basic_planには、idやnameといった
+        # キーによるバリューの呼び出しが行える
+        # @basic_plan.id == 1
+        # @basic_plan.name == basic
+        @pro_plan = Plan.find(2)
     end
     
     def about
