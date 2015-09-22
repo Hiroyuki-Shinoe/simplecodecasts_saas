2c242842b507ee33c3c32a3ef72daad7fe442872
Hiroyuki-Shinoe <yuxing0129@gmail.com>
2015-09-22 16:23:00 +0000

    Fixed typo in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  befor_filter :configure_permitted_parameters, if: :devise_controller? 
+  before_filter :configure_permitted_parameters, if: :devise_controller? 
   
   # rails 4以降、strong parameterによって、個別に記述したデータしかフォームから取得できないようになっている
   # そこで、データテーブルにカラムを増やし、そのカラムに入れるデータをフォームから取得しようとする際には、
