8e16f4788783fbc6c87b569c0ffa2fe38485f5bb
Hiroyuki-Shinoe <yuxing0129@gmail.com>
2015-09-27 12:08:16 +0000

    Modified comments

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,12 @@
 class ProfilesController < ApplicationController
     def new
         # form where a user can fill out own profile.
-        # まず、現在ページにアクセスしているユーザーを識別する。
-        # params[:user_id] でクライアント側からユーザーのuser_idが渡ってくるようにする。
-        # 
+        # まず、profileを入力しようとしているuserがどのuserか識別する処理を書く
+        # app/views/pages/home.html.erb の new_user_profile_path(current_user)
+        # から渡されたuser情報から、params[:user_id] で user_idを取得し、
+        # どのuserかを識別する。
         @user = User.find(params[:user_id])
+        # 下１行はparamsの説明のための見本
         @variable = params[:hello]
         # 上記で識別したユーザーのprofileを作成
         @profile = @user.build_profile
