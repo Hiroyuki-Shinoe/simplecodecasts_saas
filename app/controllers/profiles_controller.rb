1cfaed9c1214674c106cb2e5ee5fc69dbd5f20ac
Hiroyuki-Shinoe <yuxing0129@gmail.com>
2015-09-27 11:45:24 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,12 @@
 class ProfilesController < ApplicationController
+    def new
+        # form where a user can fill out own profile.
+        # まず、現在ページにアクセスしているユーザーを識別する。
+        # params[:user_id] でクライアント側からユーザーのuser_idが渡ってくるようにする。
+        # 
+        @user = User.find(params[:user_id])
+        @variable = params[:hello]
+        # 上記で識別したユーザーのprofileを作成
+        @profile = @user.build_profile
+    end
 end
